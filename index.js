441bfdd4d23cb5e8129ad5d648ec69c9ad1a7233
SureshKumarKummari <sureshk28591@gmail.com>
2024-03-18 06:55:22 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("header").style.borderBottom = "solid orange";
+document.getElementById("header").style.padding = "1rem 0 ";
+document.getElementById("basket-heading").style.color = "green";
+document.getElementById("thanks").innerHTML="<p>Please visit us again</p>";
